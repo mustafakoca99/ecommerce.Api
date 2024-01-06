8e6718fc5ab8840e0b4996c253d2725f556dce00
Mustafa Koca <Mustafa.Koca@Vault.Insurance>
2024-01-06 21:17:59 +0300

    added designtimedbcontextfactory

---
+++ Infrastructure/ecommerceAPI.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using ecommerceAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ecommerceAPI.Persistance
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EcommerceAPIDbContext>
+    {
+        public EcommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<EcommerceAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(
+                "server=(localdb)\\kmssqllocaldb;database=ecommerce;user=sa;password=M.k12345"
+                );
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
