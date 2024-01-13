1cd514ca7f42fce522b04a41713a955208172233
Mustafa Koca <Mustafa.Koca@Vault.Insurance>
2024-01-13 16:51:42 +0300

    updated

--- Presentation/ecommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ecommerceAPI.API/Controllers/ProductsController.cs
@@ -9,19 +9,19 @@ namespace ecommerceAPI.API.Controllers
     {
         readonly private IProductReadRepository _productReadRepository;
         readonly private IProductWriteRepository _productWriteRepository;
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository, ILogger<ProductsController> logger)
         {
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
+            _logger = logger;
         }
 
-        /// <summary>
-        /// test method
-        /// </summary>
-        /// <returns></returns>
         [HttpGet]
         public async Task Get()
         {
+            _logger.LogInformation("start get product");
+
             await _productWriteRepository.AddRangeAsync(new()
             {
                 new(){ Id = Guid.NewGuid(), Name = "test", Price = 100, Stock = 5, CreatedDate =DateTime.UtcNow },
